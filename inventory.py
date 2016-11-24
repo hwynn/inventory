93f1263debf6fbb05b65b1f5cbb1db117b099fbb
Harrison Wynn <harrisonwynn42@gmail.com>
2016-11-24 10:44:43 -0600

    error checking user input that loops instead of crashing

--- inventory.py
+++ inventory.py
@@ -1,4 +1,21 @@
+def isValid(x):
+	"Tests if the input is proper"
+	if(x.isdigit()):
+		if((int(x)>=1)and(int(x)<=5)):
+			return(True);
+		else:
+			print("Input must be between 1 and 5");
+			return(False);
+	else:
+		print("Input must a number");
+		return(False);
+
 menu = "1) Check inventory\n2) Add item to inventory\n3) Remove item from inventory"
 print(menu);
 inventory = {}
-str1 = input("Select an option: ");
+
+valid = False;
+while(valid!=True):
+	str1 = input("Select an option: ");
+	valid = isValid(str1)
+print ("Received input is : ", str1);
