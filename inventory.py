cc880999dd822f918977b9db763684e58191ac0f
Harrison Wynn <harrisonwynn42@gmail.com>
2016-12-18 20:17:29 -0600

    User input actually causes functions to run

--- inventory.py
+++ inventory.py
@@ -20,12 +20,28 @@ def invAddItem(x):
 def checkInventory():
 	print(inventory);
 
+def invRemoveItem(x):
+	print("sorry. this doesn't work yet.");
+
 menu = "1) Check inventory\n2) Add item to inventory\n3) Remove item from inventory"
 print(menu);
 inventory = {}
 
+def runner(x):
+	if(x==1):
+		checkInventory();
+	elif(x==2):
+		invAddItem(input("What will you add?: "));
+	elif(x==3):
+		invRemoveItem(input("What will you remove?: "));
+	else:
+		print("We didn't plan for this");
+
+str1 = "";
 valid = False;
 while(valid!=True):
 	str1 = input("Select an option: ");
 	valid = isValid(str1)
+runner(int(str1));
 print ("Received input is : ", str1);
+
